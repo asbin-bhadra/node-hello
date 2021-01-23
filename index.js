93a912d18d4c0f89b25787b0928f79ec35743eb1
Work <asbin.bhadra@knoldus.com>
2021-01-23 20:02:31 +0530

    chage

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
 
 const server2 = http.createServer((req, res) => {
 	res.statusCode = 200;
-	const msg2 = 'Hello again from Knoldus Software LLP by Asbin\n'
+	const msg2 = 'Hello again from Knoldus Software LLP\n'
 	res.end(msg2);
 });
 
