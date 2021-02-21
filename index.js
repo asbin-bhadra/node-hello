353ea2c6566f8f91591ba064d9c085e157e151a2
asbin-bhadra <76769957+asbin-bhadra@users.noreply.github.com>
2021-02-21 21:31:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello From Knoldus!\n'
+  const msg = 'Hello From Asbin!\n'
   res.end(msg);
 });
 
 const server2 = http.createServer((req, res) => {
 	res.statusCode = 200;
-	const msg2 = 'Hello again from Knoldus!\n'
+	const msg2 = 'Hello again from Asbin!\n'
 	res.end(msg2);
 });
 
